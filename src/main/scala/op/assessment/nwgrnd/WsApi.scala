2b2cb9495c8ac700c58cd1c5282c249fb35a922c
oleksii.prosianko <oleksii.prosianko@globallogic.com>
2017-10-01 12:23:00 +0300

    Nwgrnd. WsApi copy-pasted stub from examples.

--- src/main/scala/op/assessment/nwgrnd/WsApi.scala
+++ src/main/scala/op/assessment/nwgrnd/WsApi.scala
@@ -1,5 +1,27 @@
 package op.assessment.nwgrnd
 
-trait WsApi {
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.ws.{ BinaryMessage, Message, TextMessage }
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{ Flow, Sink, Source }
 
+trait WsApi {
+  import akka.http.scaladsl.server.Directives._
+
+  implicit val materializer: ActorMaterializer
+
+  val route: Route = path("greeter") {
+    handleWebSocketMessages(greeter)
+  }
+
+  def greeter: Flow[Message, Message, Any] =
+    Flow[Message].mapConcat {
+      case tm: TextMessage =>
+        TextMessage(Source.single("Hello ") ++ tm.textStream ++ Source.single("!")) :: Nil
+      case bm: BinaryMessage =>
+        // ignore binary messages but drain content to avoid the stream being clogged
+        bm.dataStream.runWith(Sink.ignore)
+        Nil
+    }
 }
--- src/test/scala/op/assessment/nwgrnd/WsApiSpec.scala
+++ src/test/scala/op/assessment/nwgrnd/WsApiSpec.scala
@@ -1,7 +1,38 @@
 package op.assessment.nwgrnd
 
-import org.scalatest.{Matchers, WordSpec}
+import scala.concurrent.duration._
+import akka.util.ByteString
+import akka.stream.OverflowStrategy
+import akka.stream.scaladsl.{ Flow, Sink, Source }
+import akka.http.scaladsl.model.ws.{ BinaryMessage, Message, TextMessage }
+import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.testkit.{ ScalatestRouteTest, WSProbe }
+import org.scalatest.{ Matchers, WordSpec }
 
-class WsApiSpec extends WordSpec with Matchers with WsApi {
+class WsApiSpec extends WordSpec with Matchers
+    with Directives with ScalatestRouteTest with WsApi {
 
+  "WsApi" in {
+    val wsClient = WSProbe()
+
+    // WS creates a WebSocket request for testing
+    WS("/greeter", wsClient.flow) ~> route ~>
+      check {
+        // check response for WS Upgrade headers
+        isWebSocketUpgrade shouldEqual true
+
+        // manually run a WS conversation
+        wsClient.sendMessage("Peter")
+        wsClient.expectMessage("Hello Peter!")
+
+        wsClient.sendMessage(BinaryMessage(ByteString("abcdef")))
+        wsClient.expectNoMessage(100.millis)
+
+        wsClient.sendMessage("John")
+        wsClient.expectMessage("Hello John!")
+
+        wsClient.sendCompletion()
+        wsClient.expectCompletion()
+      }
+  }
 }

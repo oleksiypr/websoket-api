5dea066f01aa884429333eae9ee6bf8601a130f5
oleksii.prosianko <oleksii.prosianko@globallogic.com>
2017-10-01 11:55:24 +0300

    Nwgrnd. Initial commit.

---
+++ src/main/scala/op/assessment/nwgrnd/WsApi.scala
@@ -0,0 +1,5 @@
+package op.assessment.nwgrnd
+
+trait WsApi {
+
+}
---
+++ src/test/scala/op/assessment/nwgrnd/WsApiSpec.scala
@@ -0,0 +1,7 @@
+package op.assessment.nwgrnd
+
+import org.scalatest.{Matchers, WordSpec}
+
+class WsApiSpec extends WordSpec with Matchers with WsApi {
+
+}
